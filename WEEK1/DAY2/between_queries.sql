1434bdc9556b5f93727875b8a88f59d69afe5371
SHAIK ZUNERA <shaikzunara@gmail.com>
2026-05-08 18:20:19 +0530

    Add SQL queries for employee salary and experience

---
+++ WEEK1/DAY2/between_queries.sql
@@ -0,0 +1,24 @@
+-- Find employees with salary BETWEEN 50000 AND 80000
+SELECT *
+FROM Employees
+WHERE salary BETWEEN 50000 AND 80000;
+
+-- Find employees with experience BETWEEN 3 AND 6
+SELECT *
+FROM Employees
+WHERE experience BETWEEN 3 AND 6;
+
+-- Find employees whose emp_id BETWEEN 105 AND 112
+SELECT *
+FROM Employees
+WHERE emp_id BETWEEN 105 AND 112;
+
+-- Find employees with salary NOT BETWEEN 40000 AND 60000
+SELECT *
+FROM Employees
+WHERE salary NOT BETWEEN 40000 AND 60000;
+
+-- Find employees with experience BETWEEN 2 AND 4
+SELECT *
+FROM Employees
+WHERE experience BETWEEN 2 AND 4;
