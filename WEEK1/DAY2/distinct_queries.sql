ee3ed18bbf5bc656ccbab83b9dec37f7f27784a2
SHAIK ZUNERA <shaikzunara@gmail.com>
2026-05-08 18:16:41 +0530

    Add SQL queries for distinct employee attributes

---
+++ WEEK1/DAY2/distinct_queries.sql
@@ -0,0 +1,19 @@
+-- Display distinct department names
+SELECT DISTINCT department
+FROM Employees;
+
+-- Display distinct city names
+SELECT DISTINCT city
+FROM Employees;
+
+-- Display distinct salary values
+SELECT DISTINCT salary
+FROM Employees;
+
+-- Display distinct combinations of department and city
+SELECT DISTINCT department, city
+FROM Employees;
+
+-- Display distinct experience values
+SELECT DISTINCT experience
+FROM Employees;
