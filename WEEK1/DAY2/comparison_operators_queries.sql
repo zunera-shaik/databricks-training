277ac8b06166d9fee2a82bf5ba3faf69fb140214
SHAIK ZUNERA <shaikzunara@gmail.com>
2026-05-08 18:17:29 +0530

    Add SQL queries for employee salary and experience

---
+++ WEEK1/DAY2/comparison_operators_queries.sql
@@ -0,0 +1,24 @@
+-- Find employees with salary >= 80000
+SELECT *
+FROM Employees
+WHERE salary >= 80000;
+
+-- Find employees with experience <= 3
+SELECT *
+FROM Employees
+WHERE experience <= 3;
+
+-- Find employees whose salary <> 45000
+SELECT *
+FROM Employees
+WHERE salary <> 45000;
+
+-- Find employees with salary < 50000
+SELECT *
+FROM Employees
+WHERE salary < 50000;
+
+-- Find employees with experience > 5
+SELECT *
+FROM Employees
+WHERE experience > 5;
